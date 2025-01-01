51ead6d8fa0d50551cbf40a4b4247f2a3338923d
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-01-01 19:05:18 +0000

    Update extract_features.py

--- utils/audio/extraction/extract_features.py
+++ utils/audio/extraction/extract_features.py
@@ -2,11 +2,9 @@
 # For more details, visit: https://creativecommons.org/licenses/by-nc/4.0/
 
 # extract_features.py
-
+import io
 import librosa
 import numpy as np
-from utils.audio.load_audio import load_and_preprocess_audio, load_audio_from_bytes
-
 
 def extract_audio_features(audio_input, sr=88200, from_bytes=False):
     if from_bytes:
@@ -64,3 +62,43 @@ def reduce_features(features):
         reduced_final_features = reduced_frames
     
     return reduced_final_features
+
+
+
+def load_and_preprocess_audio(audio_path, sr=88200):
+    y, sr = load_audio(audio_path, sr)
+    if sr != 88200:
+        y = librosa.resample(y, orig_sr=sr, target_sr=88200)
+        sr = 88200
+    
+    max_val = np.max(np.abs(y))
+    if max_val > 0:
+        y = y / max_val
+
+    return y, sr
+
+def load_audio(audio_path, sr=88200):
+    y, sr = librosa.load(audio_path, sr=sr)
+    print(f"Loaded audio file '{audio_path}' with sample rate {sr}")
+    return y, sr
+
+def load_audio_from_bytes(audio_bytes, sr=88200):
+    audio_file = io.BytesIO(audio_bytes)
+    y, sr = librosa.load(audio_file, sr=sr)
+    
+    max_val = np.max(np.abs(y))
+    if max_val > 0:
+        y = y / max_val
+
+    return y, sr
+
+def load_audio_file_from_memory(audio_bytes, sr=88200):
+    """Load audio from memory bytes."""
+    y, sr = librosa.load(io.BytesIO(audio_bytes), sr=sr)
+    print(f"Loaded audio data with sample rate {sr}")
+    
+    max_val = np.max(np.abs(y))
+    if max_val > 0:
+        y = y / max_val
+
+    return y, sr
