f23390aea6cd5d53b1603144ca8a2409876cc631
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-01-01 19:03:16 +0000

    Update extract_features.py

--- utils/audio/extraction/extract_features.py
+++ utils/audio/extraction/extract_features.py
@@ -3,9 +3,10 @@
 
 # extract_features.py
 
+import librosa
 import numpy as np
 from utils.audio.load_audio import load_and_preprocess_audio, load_audio_from_bytes
-from utils.audio.extraction.extract_features_utils import extract_mfcc_features
+
 
 def extract_audio_features(audio_input, sr=88200, from_bytes=False):
     if from_bytes:
@@ -31,3 +32,35 @@ def extract_audio_features(audio_input, sr=88200, from_bytes=False):
     combined_features = np.hstack(all_features)
     
     return combined_features, y
+
+def extract_mfcc_features(y, sr, frame_length, hop_length, num_mfcc=23):
+    mfcc_features = extract_overlapping_mfcc(y, sr, num_mfcc, frame_length, hop_length)
+    reduced_mfcc_features = reduce_features(mfcc_features)
+    return reduced_mfcc_features.T
+
+def cepstral_mean_variance_normalization(mfcc):
+    mean = np.mean(mfcc, axis=1, keepdims=True)
+    std = np.std(mfcc, axis=1, keepdims=True)
+    return (mfcc - mean) / (std + 1e-10)
+
+def extract_overlapping_mfcc(chunk, sr, num_mfcc, frame_length, hop_length, ):
+    mfcc = librosa.feature.mfcc(y=chunk, sr=sr, n_mfcc=num_mfcc + 1, n_fft=frame_length, hop_length=hop_length)
+    mfcc = cepstral_mean_variance_normalization(mfcc)
+    mfcc = mfcc[1:] 
+    delta_mfcc = librosa.feature.delta(mfcc)
+    delta2_mfcc = librosa.feature.delta(mfcc, order=2)
+    combined_mfcc = np.vstack([mfcc, delta_mfcc, delta2_mfcc])
+    return combined_mfcc    
+
+def reduce_features(features):
+    num_frames = features.shape[1]
+    paired_frames = features[:, :num_frames // 2 * 2].reshape(features.shape[0], -1, 2)
+    reduced_frames = paired_frames.mean(axis=2)
+    
+    if num_frames % 2 == 1:
+        last_frame = features[:, -1].reshape(-1, 1)
+        reduced_final_features = np.hstack((reduced_frames, last_frame))
+    else:
+        reduced_final_features = reduced_frames
+    
+    return reduced_final_features
