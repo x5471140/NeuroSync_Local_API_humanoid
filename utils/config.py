aee0434cdbda2bf3fe1ff99c67038a7921085ccd
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-01-01 16:13:04 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,6 @@ config = {
     'num_heads': 4,
     'dropout': 0.0,
     'output_dim': 68,
-    'input_dim': 26 + 26 + 26 + 1,
-    'frame_size': 128,
+    'input_dim': 69,
+    'frame_size': 128, 
 }
