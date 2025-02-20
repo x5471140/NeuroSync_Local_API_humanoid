69ce768a268a121ce0fb461e174f4d8cda056ac8
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-02-20 22:03:59 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -2,10 +2,10 @@ config = {
     'sr': 88200,
     'frame_rate': 60,
     'hidden_dim': 1024,
-    'n_layers': 4,
-    'num_heads': 4,
+    'n_layers': 4, # if your own set to 8
+    'num_heads': 4, # this should be 16 if its your own.
     'dropout': 0.0,
-    'output_dim': 68, # if you trained your own, this should be 61
+    'output_dim': 68, # if you trained your own, this should also be 61
     'input_dim': 256,
     'frame_size': 128, 
 }
