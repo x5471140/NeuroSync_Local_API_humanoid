869389d71214ed8cbf9114a92b36477ee7ea0b38
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-02-21 13:37:39 +0000

    Update config.py
    scaling UP

--- utils/config.py
+++ utils/config.py
@@ -2,8 +2,8 @@ config = {
     'sr': 88200,
     'frame_rate': 60,
     'hidden_dim': 1024,
-    'n_layers': 4, # if your own set to 8
-    'num_heads': 4, # this should be 16 if its your own.
+    'n_layers': 8, 
+    'num_heads': 16, 
     'dropout': 0.0,
     'output_dim': 68, # if you trained your own, this should also be 61
     'input_dim': 256,
