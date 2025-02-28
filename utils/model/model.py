ada076659043ee47c0f71a637f5471dc7abef71f
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-02-28 11:22:02 +0000

    Update model.py

--- utils/model/model.py
+++ utils/model/model.py
@@ -25,7 +25,6 @@ def load_model(model_path, config, device):
     n_layers = config['n_layers']
     num_heads = config['num_heads']
     
-    # Assume Encoder, Decoder, and Seq2Seq are defined elsewhere.
     encoder = Encoder(config['input_dim'], hidden_dim, n_layers, num_heads)
     decoder = Decoder(config['output_dim'], hidden_dim, n_layers, num_heads)
     model = Seq2Seq(encoder, decoder, device).to(device)
